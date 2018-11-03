e3df3011f2ba10da844322b8aa353ba3aec1839b
Jen Hoskins <jen.c.hoskins@gmail.com>
2018-11-03 13:24:49 +0000

    Set up Gulp for Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,23 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+let watch = require("gulp-watch-sass");
+let rename = require("gulp-rename");
 
-gulp.task('default', []);
+gulp.task("sass", function() {
+  return gulp
+    .src("./src/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./src/"))
+    .pipe(
+      rename({
+        extname: ".css"
+      })
+    );
+});
+
+gulp.task("watch", function() {
+  console.log("working");
+  gulp.watch("./src/*.scss", ["sass"]);
+});
+
+gulp.task("default", []);
